231915a38e2963fe6de87f2af50f714a531763bd
miroslav-torodov <tredmus@protonmail.com>
2023-01-14 02:47:49 +0200

    Horizontal Scroll

--- src/Components/Products/Products.tsx
+++ src/Components/Products/Products.tsx
@@ -1,10 +1,38 @@
+import { useEffect, useRef } from "react";
+import { useDraggable } from "react-use-draggable-scroll";
 import classes from "./Products.module.scss";
 import { Product } from "../Product/Product";
 import products from "../../Data/products.json";
 
 export const Products = () => {
+  const ref =
+    useRef<HTMLDivElement>() as unknown as React.MutableRefObject<HTMLUListElement>;
+  const { events } = useDraggable(ref);
+
+  function useHorizontalScroll() {
+    const elRef: any = useRef();
+    useEffect(() => {
+      const el = ref.current;
+      if (el) {
+        const onWheel = (e: any) => {
+          if (e.deltaY == 0) return;
+          e.preventDefault();
+          el.scrollTo({
+            left: el.scrollLeft + e.deltaY,
+            // behavior: "smooth",
+          });
+        };
+        el.addEventListener("wheel", onWheel);
+        return () => el.removeEventListener("wheel", onWheel);
+      }
+    }, []);
+    return ref;
+  }
+
+  const scrollRef = useHorizontalScroll();
+
   return (
-    <ul className={classes.products}>
+    <ul className={classes.products} {...events} ref={scrollRef}>
       {products.map((product: any) => {
         return (
           <Product
